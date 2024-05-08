18c44fa657de536d9bf757fd9efefc1f6d0a104f
crazyupinc-design <crazyupinc-design>
2024-05-08 17:17:45 +0000

    Pushed from Locofy Builder

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,22 @@ import type { NextPage } from "next";
 
 const Index1: NextPage = () => {
   return (
-    <div className="w-full relative bg-bg-colors-white h-[22.5rem] overflow-hidden flex flex-col items-start justify-start p-[2.5rem] box-border text-center text-[2.25rem] text-black font-headings-headling-2">
-      <b className="relative leading-[2.5rem]">index</b>
+    <div className="w-full relative bg-bg-colors-white overflow-hidden flex flex-col items-start justify-start p-[2.5rem] box-border gap-[1.25rem] text-left text-[2.25rem] text-black font-headings-heading-3">
+      <b className="self-stretch relative leading-[2.5rem]">index</b>
+      <a
+        className="self-stretch relative [text-decoration:underline] leading-[2.5rem] font-bold text-[inherit]"
+        href="http://localhost:3000/skeleton-sub"
+        target="_blank"
+      >
+        http://localhost:3000/skeleton-sub
+      </a>
+      <a
+        className="self-stretch relative [text-decoration:underline] leading-[2.5rem] font-bold text-[inherit]"
+        href="http://localhost:3000/skeleton-sub-example"
+        target="_blank"
+      >
+        http://localhost:3000/skeleton-sub-example
+      </a>
     </div>
   );
 };
