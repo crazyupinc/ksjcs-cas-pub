b5f403393ecccff999a87f93e67a67d0f8aa9b7b
crazyupinc-design <crazyupinc-design>
2024-05-09 15:18:01 +0000

    Pushed from Locofy Builder

--- components/page-title.tsx
+++ components/page-title.tsx
@@ -2,15 +2,15 @@ import type { NextPage } from "next";
 
 const PageTitle: NextPage = () => {
   return (
-    <div className="w-[1920px] bg-bg-colors-white max-w-full h-[220px] flex flex-col items-center justify-center py-[50px] px-5 box-border text-center text-11xl text-text-colors-dark-gray font-sub-main-title">
+    <div className="w-[1920px] bg-bg-colors-white max-w-full h-[180px] flex flex-col items-center justify-center py-[50px] px-5 box-border text-center text-11xl text-text-colors-dark-gray font-body-text-caption-bold">
       <div className="self-stretch flex flex-col items-center justify-start gap-[6px]">
         <div className="flex flex-row items-start justify-center py-0 pr-[23px] pl-0 gap-[6px]">
           <img
             className="w-[23px] relative h-[22px] object-cover"
             alt=""
-            src="/logo-1@2x.png"
+            src="/logo@2x.png"
           />
-          <div className="w-[113px] relative leading-[36px] font-extrabold flex items-end justify-center h-10 shrink-0">
+          <div className="w-[113px] relative leading-[36px] font-extrabold flex items-end justify-center h-10 shrink-0 sm:text-5xl">
             회원가입
           </div>
         </div>
--- components/select-member-type.tsx
+++ components/select-member-type.tsx
@@ -4,14 +4,14 @@ const SelectMemberType: NextPage = () => {
   return (
     <div className="w-[1920px] bg-bg-colors-white max-w-full overflow-hidden flex flex-row items-start justify-center p-5 box-border text-center text-sm text-dark-gray font-body-text-caption-bold md:flex-col">
       <div className="self-stretch flex-1 flex flex-col items-center justify-start md:flex-[unset] md:self-stretch">
-        <div className="self-stretch flex flex-col items-center justify-start py-20 px-0 gap-[36px]">
+        <div className="self-stretch flex flex-col items-center justify-start py-20 px-0 gap-[36px] sm:pt-5 sm:pb-5 sm:box-border">
           <img
-            className="w-[344.4px] relative h-[346px] object-cover"
+            className="w-[344.4px] relative h-[346px] object-cover sm:w-[300px] sm:min-w-[300px]"
             alt=""
             src="/usergroupicon@2x.png"
           />
           <div className="self-stretch flex flex-col items-center justify-start gap-[8px]">
-            <div className="self-stretch relative text-17xl leading-[40px] font-extrabold">
+            <div className="self-stretch h-7 relative text-5xl font-extrabold inline-block sm:text-[22px]">
               회원 유형 선택
             </div>
             <div className="self-stretch relative">
@@ -23,12 +23,12 @@ const SelectMemberType: NextPage = () => {
           </div>
           <div className="self-stretch flex flex-row flex-wrap items-start justify-center gap-[16px]">
             <button className="cursor-pointer [border:none] py-3.5 px-7 bg-button-colors-dark-sky rounded-md overflow-hidden flex flex-row items-center justify-center hover:bg-royalblue">
-              <div className="relative text-base font-body-text-caption-bold text-bg-colors-white text-center">
+              <div className="relative text-base font-body-text-caption-bold text-bg-colors-white text-center sm:text-smi">
                 개인회원 가입
               </div>
             </button>
             <button className="cursor-pointer [border:none] py-3.5 px-7 bg-button-colors-dark-sky rounded-md overflow-hidden flex flex-row items-center justify-center hover:bg-royalblue">
-              <div className="relative text-base font-body-text-caption-bold text-bg-colors-white text-center">
+              <div className="relative text-base font-body-text-caption-bold text-bg-colors-white text-center sm:text-smi">
                 기관회원 가입
               </div>
             </button>
