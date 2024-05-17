c22b2dc145d8c1f2878e61e8e36026e37d5209d1
crazyupinc-design <crazyupinc-design>
2024-05-17 04:30:39 +0000

    Pushed from Locofy Builder

--- components/find-form.tsx
+++ components/find-form.tsx
@@ -1,43 +1,161 @@
 import type { NextPage } from "next";
-import InputField from "./input-field";
+import { useMemo, type CSSProperties } from "react";
 
-const FindForm: NextPage = () => {
+export type FindFormType = {
+  /** Style props */
+  themeFontSize1?: CSSProperties["fontSize"];
+  themeLineHeight1?: CSSProperties["lineHeight"];
+  themeFontSize2?: CSSProperties["fontSize"];
+  themeLineHeight2?: CSSProperties["lineHeight"];
+  themeFontSize3?: CSSProperties["fontSize"];
+  themeLineHeight3?: CSSProperties["lineHeight"];
+  divFontSize?: CSSProperties["fontSize"];
+  divLineHeight?: CSSProperties["lineHeight"];
+  formGap?: CSSProperties["gap"];
+  inputGap?: CSSProperties["gap"];
+  inputFieldPadding?: CSSProperties["padding"];
+  inputFieldFontSize?: CSSProperties["fontSize"];
+  inputFieldFontSize1?: CSSProperties["fontSize"];
+  inputFieldPadding1?: CSSProperties["padding"];
+  buttonPadding?: CSSProperties["padding"];
+  buttonFontSize?: CSSProperties["fontSize"];
+  buttonLineHeight?: CSSProperties["lineHeight"];
+};
+
+const FindForm: NextPage<FindFormType> = ({
+  themeFontSize1,
+  themeLineHeight1,
+  themeFontSize2,
+  themeLineHeight2,
+  themeFontSize3,
+  themeLineHeight3,
+  divFontSize,
+  divLineHeight,
+  formGap,
+  inputGap,
+  inputFieldPadding,
+  inputFieldFontSize,
+  inputFieldFontSize1,
+  inputFieldPadding1,
+  buttonPadding,
+  buttonFontSize,
+  buttonLineHeight,
+}) => {
+  const themeStyle: CSSProperties = useMemo(() => {
+    return {
+      fontSize: themeFontSize1,
+      lineHeight: themeLineHeight1,
+    };
+  }, [themeFontSize1, themeLineHeight1]);
+
+  const theme1Style: CSSProperties = useMemo(() => {
+    return {
+      fontSize: themeFontSize2,
+      lineHeight: themeLineHeight2,
+    };
+  }, [themeFontSize2, themeLineHeight2]);
+
+  const theme2Style: CSSProperties = useMemo(() => {
+    return {
+      fontSize: themeFontSize3,
+      lineHeight: themeLineHeight3,
+    };
+  }, [themeFontSize3, themeLineHeight3]);
+
+  const divStyle: CSSProperties = useMemo(() => {
+    return {
+      fontSize: divFontSize,
+      lineHeight: divLineHeight,
+    };
+  }, [divFontSize, divLineHeight]);
+
+  const formStyle: CSSProperties = useMemo(() => {
+    return {
+      gap: formGap,
+    };
+  }, [formGap]);
+
+  const inputStyle: CSSProperties = useMemo(() => {
+    return {
+      gap: inputGap,
+    };
+  }, [inputGap]);
+
+  const inputFieldStyle: CSSProperties = useMemo(() => {
+    return {
+      padding: inputFieldPadding,
+      fontSize: inputFieldFontSize,
+    };
+  }, [inputFieldPadding, inputFieldFontSize]);
+
+  const inputField1Style: CSSProperties = useMemo(() => {
+    return {
+      fontSize: inputFieldFontSize1,
+      padding: inputFieldPadding1,
+    };
+  }, [inputFieldFontSize1, inputFieldPadding1]);
+
+  const button1Style: CSSProperties = useMemo(() => {
+    return {
+      padding: buttonPadding,
+    };
+  }, [buttonPadding]);
+
+  const button2Style: CSSProperties = useMemo(() => {
+    return {
+      fontSize: buttonFontSize,
+      lineHeight: buttonLineHeight,
+    };
+  }, [buttonFontSize, buttonLineHeight]);
+
   return (
-    <div className="w-[600px] rounded-xl bg-bg-white max-w-[600px] flex flex-col items-center justify-start py-9 px-4 box-border gap-[40px] min-w-[300px] text-left text-sm text-text-text-small-light font-body-text-normal-text">
+    <div className="w-[600px] rounded-xl bg-bg-colors-white max-w-[600px] flex flex-col items-center justify-start py-9 px-4 box-border gap-[40px] min-w-[300px] text-left text-sm text-text-text-small-light font-headings-headling-2">
       <div className="box-border flex flex-row items-center justify-center gap-[6px] min-w-[150px] border-b-[1px] border-solid border-bg-light-border">
         <button className="cursor-pointer [border:none] pt-0 px-0 pb-0.5 bg-[transparent] self-stretch hidden flex-col items-center justify-center">
-          <div className="self-stretch flex-1 relative text-xs leading-[16px] font-body-text-normal-text text-text-gray text-center flex items-center justify-center">
+          <div className="self-stretch flex-1 relative text-xs leading-[16px] font-headings-headling-2 text-text-gray text-center flex items-center justify-center">
             Account
           </div>
         </button>
         <button className="cursor-pointer [border:none] p-0 bg-[transparent] h-7 flex flex-col items-center justify-center">
-          <div className="self-stretch flex-1 relative text-xs leading-[16px] font-extrabold font-body-text-normal-text text-text-dark-gray text-center flex items-center justify-center">
+          <b className="self-stretch flex-1 relative text-xs leading-[16px] flex font-headings-headling-2 text-text-dark-gray text-center items-center justify-center">
             아이디 찾기
-          </div>
-          <div className="self-stretch relative bg-bg-primary h-0.5" />
+          </b>
+          <div className="self-stretch relative bg-button-colors-dark-sky h-0.5" />
         </button>
         <button className="cursor-pointer [border:none] pt-0 px-0 pb-0.5 bg-[transparent] h-7 flex flex-col items-center justify-center box-border">
-          <div className="self-stretch flex-1 relative text-xs leading-[16px] font-extrabold font-body-text-normal-text text-text-lightest-gray text-center flex items-center justify-center">
+          <b className="self-stretch flex-1 relative text-xs leading-[16px] flex font-headings-headling-2 text-text-hint-text text-center items-center justify-center">
             비밀번호 찾기
-          </div>
+          </b>
         </button>
         <button className="cursor-pointer [border:none] p-0 bg-[transparent] self-stretch hidden flex-col items-center justify-center">
-          <div className="self-stretch flex-1 relative text-xs leading-[16px] font-body-text-normal-text text-text-gray text-center flex items-center justify-center">
+          <div
+            className="self-stretch flex-1 relative text-xs leading-[16px] font-headings-headling-2 text-text-gray text-center flex items-center justify-center"
+            style={themeStyle}
+          >
             Theme
           </div>
         </button>
         <button className="cursor-pointer [border:none] p-0 bg-[transparent] self-stretch hidden flex-col items-center justify-center">
-          <div className="self-stretch flex-1 relative text-xs leading-[16px] font-body-text-normal-text text-text-gray text-center flex items-center justify-center">
+          <div
+            className="self-stretch flex-1 relative text-xs leading-[16px] font-headings-headling-2 text-text-gray text-center flex items-center justify-center"
+            style={theme1Style}
+          >
             Theme
           </div>
         </button>
         <button className="cursor-pointer [border:none] p-0 bg-[transparent] self-stretch hidden flex-col items-center justify-center">
-          <div className="self-stretch flex-1 relative text-xs leading-[16px] font-body-text-normal-text text-text-gray text-center flex items-center justify-center">
+          <div
+            className="self-stretch flex-1 relative text-xs leading-[16px] font-headings-headling-2 text-text-gray text-center flex items-center justify-center"
+            style={theme2Style}
+          >
             Theme
           </div>
         </button>
       </div>
-      <div className="w-full relative leading-[20px] inline-block max-w-[528px]">
+      <div
+        className="w-full relative leading-[20px] inline-block max-w-[528px]"
+        style={divStyle}
+      >
         <p className="m-0">
           회원가입 시 등록한 이름과 핸드폰 번호를 입력해주세요.
         </p>
@@ -45,13 +163,35 @@ const FindForm: NextPage = () => {
           입력한 정보가 일치하면 아이디 정보를 확인하실 수 있습니다.
         </p>
       </div>
-      <div className="w-full flex flex-col items-center justify-start gap-[24px] max-w-[528px]">
-        <div className="self-stretch flex flex-col items-center justify-start gap-[14px]">
-          <InputField inputFieldPlaceholder="이름을 입력해주세요." />
-          <InputField inputFieldPlaceholder="핸드폰 번호를 입력해주세요." />
+      <div
+        className="w-full flex flex-col items-center justify-start gap-[24px] max-w-[528px]"
+        style={formStyle}
+      >
+        <div
+          className="self-stretch flex flex-col items-center justify-start gap-[14px]"
+          style={inputStyle}
+        >
+          <input
+            className="[outline:none] bg-bg-colors-white self-stretch rounded flex flex-col items-start justify-start py-2.5 px-3.5 font-headings-headling-2 text-sm text-text-dark-gray border-[1px] border-solid border-bg-light-border"
+            placeholder="이름을 입력해주세요."
+            type="text"
+            style={inputFieldStyle}
+          />
+          <input
+            className="[outline:none] font-headings-headling-2 text-sm bg-bg-colors-white self-stretch rounded flex flex-col items-start justify-start py-2.5 px-3.5 text-text-dark-gray border-[1px] border-solid border-bg-light-border"
+            placeholder="이름을 입력해주세요."
+            type="text"
+            style={inputField1Style}
+          />
         </div>
-        <button className="cursor-pointer [border:none] py-3.5 px-7 bg-bg-primary self-stretch rounded-md overflow-hidden flex flex-row items-center justify-center hover:bg-royalblue mq420:py-2.5 mq420:px-4 mq420:box-border">
-          <div className="relative text-base leading-[24px] font-body-text-normal-text text-bg-white text-center mq420:text-smi">
+        <button
+          className="cursor-pointer [border:none] py-3.5 px-7 bg-button-colors-dark-sky self-stretch rounded-md overflow-hidden flex flex-row items-center justify-center hover:bg-royalblue mq420:py-2.5 mq420:px-4 mq420:box-border"
+          style={button1Style}
+        >
+          <div
+            className="relative text-base leading-[24px] font-headings-headling-2 text-bg-colors-white text-center mq420:text-smi"
+            style={button2Style}
+          >
             아이디 찾기
           </div>
         </button>
--- pages/find-pw.tsx
+++ pages/find-pw.tsx
@@ -1,52 +1,55 @@
 import type { NextPage } from "next";
-import InputField from "../components/input-field";
 
 const FindPw: NextPage = () => {
   return (
-    <div className="w-full relative bg-semantic-accent-light flex flex-col items-center justify-center p-[1.25rem] box-border text-left text-[0.875rem] text-text-text-small-light font-body-text-normal-text">
+    <div className="w-full relative bg-colors-blue-50 flex flex-col items-center justify-center p-[1.25rem] box-border text-left text-[0.875rem] text-text-text-small-light font-headings-headling-2">
       <div className="self-stretch flex flex-col items-center justify-start py-[3.75rem] px-[0rem] gap-[1.625rem]">
         <img
           className="w-[12.5rem] relative h-[3.956rem] object-cover"
           alt=""
           src="/logo@2x.png"
         />
-        <div className="w-full rounded-xl bg-bg-white flex flex-col items-center justify-start py-[2.25rem] px-[1rem] box-border gap-[2.5rem] min-w-[18.75rem] max-w-[37.5rem]">
+        <div className="w-full rounded-xl bg-bg-colors-white flex flex-col items-center justify-start py-[2.25rem] px-[1rem] box-border gap-[2.5rem] min-w-[18.75rem] max-w-[37.5rem]">
           <div className="box-border flex flex-row items-center justify-center gap-[0.375rem] min-w-[9.375rem] border-b-[1px] border-solid border-bg-light-border">
             <button className="cursor-pointer [border:none] pt-[0rem] px-[0rem] pb-[0.125rem] bg-[transparent] self-stretch flex flex-col items-center justify-center">
-              <div className="self-stretch flex-1 relative text-[0.75rem] leading-[1rem] font-extrabold font-body-text-normal-text text-text-lightest-gray text-center flex items-center justify-center">
+              <b className="self-stretch flex-1 relative text-[0.75rem] leading-[1rem] flex font-headings-headling-2 text-text-hint-text text-center items-center justify-center">
                 아이디 찾기
-              </div>
+              </b>
             </button>
             <button className="cursor-pointer [border:none] p-0 bg-[transparent] h-[1.75rem] flex flex-col items-center justify-center">
-              <div className="self-stretch flex-1 relative text-[0.75rem] leading-[1rem] font-extrabold font-body-text-normal-text text-text-dark-gray text-center flex items-center justify-center">
+              <b className="self-stretch flex-1 relative text-[0.75rem] leading-[1rem] flex font-headings-headling-2 text-text-dark-gray text-center items-center justify-center">
                 비밀번호 찾기
-              </div>
-              <div className="self-stretch relative bg-bg-primary h-[0.125rem]" />
+              </b>
+              <div className="self-stretch relative bg-button-colors-dark-sky h-[0.125rem]" />
             </button>
             <button className="cursor-pointer [border:none] pt-[0rem] px-[0rem] pb-[0.125rem] bg-[transparent] h-[1.75rem] hidden flex-col items-center justify-center box-border">
-              <div className="self-stretch flex-1 relative text-[0.75rem] leading-[1rem] font-extrabold font-body-text-normal-text text-text-lightest-gray text-center flex items-center justify-center">
+              <b className="self-stretch flex-1 relative text-[0.75rem] leading-[1rem] flex font-headings-headling-2 text-text-hint-text text-center items-center justify-center">
                 비밀번호 찾기
-              </div>
+              </b>
             </button>
             <button className="cursor-pointer [border:none] p-0 bg-[transparent] self-stretch hidden flex-col items-center justify-center">
-              <div className="self-stretch flex-1 relative text-[0.75rem] leading-[1rem] font-body-text-normal-text text-text-gray text-center flex items-center justify-center">
+              <div className="self-stretch flex-1 relative text-[0.75rem] leading-[1rem] font-headings-headling-2 text-text-gray text-center flex items-center justify-center">
                 Theme
               </div>
             </button>
             <button className="cursor-pointer [border:none] p-0 bg-[transparent] self-stretch hidden flex-col items-center justify-center">
-              <div className="self-stretch flex-1 relative text-[0.75rem] leading-[1rem] font-body-text-normal-text text-text-gray text-center flex items-center justify-center">
+              <div className="self-stretch flex-1 relative text-[0.75rem] leading-[1rem] font-headings-headling-2 text-text-gray text-center flex items-center justify-center">
                 Theme
               </div>
             </button>
             <button className="cursor-pointer [border:none] p-0 bg-[transparent] self-stretch hidden flex-col items-center justify-center">
-              <div className="self-stretch flex-1 relative text-[0.75rem] leading-[1rem] font-body-text-normal-text text-text-gray text-center flex items-center justify-center">
+              <div className="self-stretch flex-1 relative text-[0.75rem] leading-[1rem] font-headings-headling-2 text-text-gray text-center flex items-center justify-center">
                 Theme
               </div>
             </button>
           </div>
           <div className="w-full relative leading-[1.25rem] inline-block max-w-[33rem]">
             <p className="m-0">
-              회원가입 시 등록한 아이디와 이메일을 입력해주세요.
+              <span className="text-text-text-small-light">{`회원가입 시 등록한 `}</span>
+              <span className="text-button-colors-dark-sky">
+                아이디와 이메일
+              </span>
+              <span>을 입력해주세요.</span>
             </p>
             <p className="m-0">
               정보가 일치하면 입력한 이메일주소로 임시 비밀번호를 알려드립니다.
@@ -54,20 +57,30 @@ const FindPw: NextPage = () => {
           </div>
           <div className="w-full flex flex-col items-center justify-start gap-[1.5rem] max-w-[33rem]">
             <div className="self-stretch flex flex-col items-center justify-start gap-[0.875rem]">
-              <InputField inputFieldPlaceholder="아이디를 입력해주세요." />
-              <InputField inputFieldPlaceholder="이메일을 입력해주세요." />
+              <input
+                className="[outline:none] font-headings-headling-2 text-[0.875rem] bg-bg-colors-white self-stretch rounded flex flex-col items-start justify-start py-[0.625rem] px-[0.875rem] text-text-dark-gray border-[1px] border-solid border-bg-light-border"
+                placeholder="아이디를 입력해주세요."
+                type="text"
+              />
+              <input
+                className="[outline:none] font-headings-headling-2 text-[0.875rem] bg-bg-colors-white self-stretch rounded flex flex-col items-start justify-start py-[0.625rem] px-[0.875rem] text-text-dark-gray border-[1px] border-solid border-bg-light-border"
+                placeholder="이메일 주소를 입력해주세요."
+                type="text"
+              />
             </div>
-            <button className="cursor-pointer [border:none] py-[0.875rem] px-[1.75rem] bg-bg-primary self-stretch rounded-md overflow-hidden flex flex-row items-center justify-center hover:bg-royalblue mq420:py-[0.625rem] mq420:px-[1rem] mq420:box-border">
-              <div className="relative text-[1rem] leading-[1.5rem] font-body-text-normal-text text-bg-white text-center mq420:text-[0.813rem]">
+            <button className="cursor-pointer [border:none] py-[0.875rem] px-[1.75rem] bg-button-colors-dark-sky self-stretch rounded-md overflow-hidden flex flex-row items-center justify-center hover:bg-royalblue mq420:py-[0.625rem] mq420:px-[1rem] mq420:box-border">
+              <div className="relative text-[1rem] leading-[1.5rem] font-headings-headling-2 text-bg-colors-white text-center mq420:text-[0.813rem]">
                 비밀번호 찾기
               </div>
             </button>
           </div>
         </div>
-        <div className="self-stretch flex flex-col items-center justify-center gap-[0.375rem] text-center text-text-lightest-gray">
+        <div className="self-stretch flex flex-col items-center justify-center gap-[0.375rem] text-center text-text-hint-text">
           <a className="[text-decoration:none] self-stretch relative leading-[1.25rem] text-[inherit] mq420:text-[0.813rem]">
             <span>{`대표 이메일 `}</span>
-            <span className="text-bg-primary">cybercom2013@naver.com</span>
+            <span className="text-button-colors-dark-sky">
+              cybercom2013@naver.com
+            </span>
           </a>
           <div className="self-stretch flex flex-row flex-wrap items-center justify-center">
             <div className="relative leading-[1.25rem] mq420:text-[0.813rem]">
