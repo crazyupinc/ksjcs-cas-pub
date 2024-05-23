3c0ba4b85f9a76b3a69328d57ab4884f8c3a5cd1
crazyupinc-design <crazyupinc-design>
2024-05-23 03:01:42 +0000

    Pushed from Locofy Builder

--- components/log-in-modal.tsx
+++ components/log-in-modal.tsx
@@ -1,22 +1,29 @@
 import type { NextPage } from "next";
+import Button from "./button";
 
-const LogInModal: NextPage = () => {
+export type LogInModalType = {
+  className?: string;
+};
+
+const LogInModal: NextPage<LogInModalType> = ({ className = "" }) => {
   return (
-    <div className="w-[400px] shadow-[0px_3px_16px_rgba(0,_0,_0,_0.08)] rounded-lg bg-bg-white max-w-[400px] overflow-hidden flex flex-col items-center justify-start py-2 px-0 box-border relative text-left text-sm text-text-text-small-light font-body-text-normal-text">
+    <div
+      className={`shadow-[0px_3px_16px_rgba(0,_0,_0,_0.08)] rounded-lg bg-bg-white overflow-hidden flex flex-col items-center justify-start py-2 px-0 box-border relative min-w-[320px] max-w-[400px] text-left text-sm text-text-text-small-light font-headings-heading-3 ${className}`}
+    >
       <img
         className="w-[196px] relative h-[62px] object-cover z-[0]"
         alt=""
         src="/logo1@2x.png"
       />
-      <div className="self-stretch flex flex-col items-start justify-start p-6 gap-[20px] z-[1]">
+      <div className="w-full flex flex-col items-start justify-start p-6 box-border gap-[20px] min-w-[320px] max-w-[400px] z-[1]">
         <div className="self-stretch flex flex-col items-start justify-start gap-[14px]">
           <input
-            className="[outline:none] font-body-text-normal-text text-sm bg-bg-white self-stretch rounded flex flex-col items-start justify-start py-2.5 px-3.5 text-text-dark-gray border-[1px] border-solid border-bg-light-border"
+            className="[outline:none] font-headings-heading-3 text-sm bg-bg-white self-stretch rounded flex flex-col items-start justify-start py-2.5 px-3.5 text-text-dark-gray border-[1px] border-solid border-bg-light-border"
             placeholder="아이디를 입력하세요."
             type="text"
           />
           <input
-            className="[outline:none] bg-bg-white self-stretch rounded flex flex-col items-start justify-start py-2.5 px-3.5 font-body-text-normal-text text-sm text-text-dark-gray border-[1px] border-solid border-bg-light-border"
+            className="[outline:none] bg-bg-white self-stretch rounded flex flex-col items-start justify-start py-2.5 px-3.5 font-headings-heading-3 text-sm text-text-dark-gray border-[1px] border-solid border-bg-light-border"
             placeholder="비밀번호를 입력하세요"
             type="password"
           />
@@ -25,11 +32,7 @@ const LogInModal: NextPage = () => {
             <div className="relative leading-[20px]">비밀번호 표시</div>
           </div>
         </div>
-        <button className="cursor-pointer [border:none] py-3.5 px-7 bg-bg-primary self-stretch rounded-md overflow-hidden flex flex-row items-center justify-center hover:bg-royalblue sm:py-2.5 sm:px-4 sm:box-border mq420:py-2.5 mq420:px-4 mq420:box-border">
-          <div className="relative text-base leading-[24px] font-body-text-normal-text text-bg-white text-center sm:text-smi">
-            로그인
-          </div>
-        </button>
+        <Button buttonAlignSelf="stretch" button="로그인" />
         <div className="self-stretch flex flex-row items-start justify-center gap-[20px] text-center">
           <div className="relative leading-[20px]">회원가입</div>
           <div className="relative leading-[20px] text-left">
