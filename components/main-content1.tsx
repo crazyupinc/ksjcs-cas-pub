7c8df1c41dca8011e538298127d890555d8e7912
crazyupinc-design <crazyupinc-design>
2024-06-03 02:59:54 +0000

    Pushed from Locofy Builder

--- components/main-content1.tsx
+++ components/main-content1.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from "next";
 
-const MainContent1: NextPage = () => {
+export type MainContent1Type = {
+  className?: string;
+};
+
+const MainContent1: NextPage<MainContent1Type> = ({ className = "" }) => {
   return (
-    <div className="w-[1920px] bg-bg-light max-w-full flex flex-col items-center justify-center py-0 px-5 box-border text-left text-17xl text-text-colors-gray font-headings-headling-2">
+    <div
+      className={`w-full bg-bg-light max-w-full flex flex-col items-center justify-center py-0 px-5 box-border text-left text-17xl text-text-title-medium font-body-text-small-text-bold ${className}`}
+    >
       <div className="w-full rounded-lg overflow-hidden flex flex-row items-center justify-center py-[90px] px-0 box-border gap-[8px] max-w-[1300px] md:flex-col md:gap-[16px] md:items-center md:justify-start sm:pt-[60px] sm:pb-[60px] sm:box-border mq420:pt-[60px] mq420:pb-[60px] mq420:box-border">
         <div className="flex-1 flex flex-row items-center justify-center md:flex-[unset] md:self-stretch">
           <img
@@ -11,28 +17,28 @@ const MainContent1: NextPage = () => {
             src="/dbpiaimg@2x.png"
           />
         </div>
-        <div className="self-stretch flex-1 flex flex-col items-start justify-center gap-[24px] md:items-start md:justify-start md:flex-[unset] md:self-stretch">
+        <div className="self-stretch flex-1 flex flex-col items-start justify-center gap-[36px] md:items-start md:justify-start md:flex-[unset] md:self-stretch">
           <div className="self-stretch flex flex-col items-start justify-start gap-[18px]">
             <b className="self-stretch relative leading-[40px] md:text-center sm:text-7xl mq420:text-5xl">
               사이버커뮤니케이션 학보
             </b>
-            <div className="self-stretch flex flex-col items-start justify-start text-base text-text-title-small-light md:items-center md:justify-center mq420:flex-col mq420:items-center mq420:justify-start">
-              <div className="relative leading-[24px] md:text-left sm:text-smi">
+            <div className="self-stretch flex flex-col items-start justify-start text-sm text-text-text-small-light md:items-center md:justify-center mq420:flex-col mq420:items-center mq420:justify-start">
+              <div className="relative leading-[20px] md:text-left sm:text-smi">
                 사이버커뮤니케이션학회 제1호(1997)
               </div>
-              <div className="relative leading-[24px] md:text-left sm:text-smi">
+              <div className="relative leading-[20px] md:text-left sm:text-smi">
                 제40권 제4호(2023) KCI등재
               </div>
             </div>
           </div>
           <div className="self-stretch flex flex-row items-start justify-start gap-[8px] md:items-start md:justify-center">
-            <button className="cursor-pointer py-3.5 px-7 bg-[transparent] rounded-md overflow-hidden flex flex-row items-center justify-center border-[1px] border-solid border-button-colors-dark-sky hover:bg-gainsboro sm:py-2 sm:px-4 sm:box-border">
-              <div className="relative text-base leading-[24px] font-headings-headling-2 text-button-colors-dark-sky text-center sm:text-smi">
+            <button className="cursor-pointer py-3.5 px-7 bg-[transparent] rounded-md overflow-hidden flex flex-row items-center justify-center border-[1px] border-solid border-bg-primary hover:bg-gainsboro-100 sm:py-2 sm:px-4 sm:box-border">
+              <div className="relative text-base leading-[24px] font-body-text-small-text-bold text-bg-primary text-center sm:text-smi">
                 논문투고 안내
               </div>
             </button>
             <button className="cursor-pointer [border:none] py-3.5 px-7 bg-text-colors-amber rounded-md overflow-hidden flex flex-row items-center justify-center hover:bg-royalblue sm:py-2 sm:px-4 sm:box-border">
-              <div className="relative text-base leading-[24px] font-headings-headling-2 text-bg-colors-white text-center sm:text-smi">
+              <div className="relative text-base leading-[24px] font-body-text-small-text-bold text-bg-white text-center sm:text-smi">
                 온라인 논문투고
               </div>
             </button>
--- components/main-content2.tsx
+++ components/main-content2.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from "next";
 
-const MainContent2: NextPage = () => {
+export type MainContent2Type = {
+  className?: string;
+};
+
+const MainContent2: NextPage<MainContent2Type> = ({ className = "" }) => {
   return (
-    <div className="w-[1920px] bg-bg-colors-white max-w-full flex flex-col items-center justify-start py-[120px] px-5 box-border gap-[24px] text-center text-17xl text-text-colors-gray font-headings-headling-2">
+    <div
+      className={`w-full bg-bg-white max-w-full flex flex-col items-center justify-start py-[110px] px-5 box-border gap-[36px] text-center text-17xl text-text-title-medium font-body-text-small-text-bold ${className}`}
+    >
       <div className="flex flex-row items-start justify-center gap-[2px] sm:flex-col sm:items-center sm:justify-center">
         <b className="relative leading-[40px] sm:text-7xl mq420:text-5xl">{`사이버 커뮤니케이션 `}</b>
         <b className="relative leading-[40px] sm:text-7xl mq420:text-5xl">
@@ -10,46 +16,31 @@ const MainContent2: NextPage = () => {
         </b>
       </div>
       <div className="flex flex-row items-center justify-start gap-[12px] border-b-[1px] border-solid border-bg-light-border">
-        <button className="cursor-pointer [border:none] p-0 bg-[transparent] self-stretch hidden flex-col items-center justify-center">
-          <div className="self-stretch flex-1 relative text-xs leading-[16px] font-headings-headling-2 text-text-gray text-center flex items-center justify-center">
-            Account
-          </div>
-        </button>
         <button className="cursor-pointer [border:none] p-0 bg-[transparent] self-stretch flex flex-col items-center justify-center">
-          <b className="self-stretch flex-1 relative text-xs leading-[16px] flex font-headings-headling-2 text-text-dark-gray text-center items-center justify-center">
+          <b className="self-stretch flex-1 relative text-sm leading-[20px] flex font-body-text-small-text-bold text-text-dark-gray text-center items-center justify-center">
             공지사항
           </b>
-          <div className="self-stretch relative bg-button-colors-dark-sky h-px" />
-        </button>
-        <button className="cursor-pointer [border:none] p-0 bg-[transparent] self-stretch hidden flex-col items-center justify-center">
-          <div className="self-stretch flex-1 relative text-xs leading-[16px] font-headings-headling-2 text-text-gray text-center flex items-center justify-center">
-            Theme
-          </div>
+          <div className="self-stretch relative bg-bg-primary h-0.5" />
         </button>
         <button className="cursor-pointer [border:none] p-0 bg-[transparent] self-stretch flex flex-col items-center justify-center">
-          <div className="self-stretch flex-1 relative text-xs leading-[16px] font-headings-headling-2 text-text-gray text-center flex items-center justify-center">
+          <div className="self-stretch flex-1 relative text-sm leading-[20px] font-body-text-small-text-bold text-text-gray text-center flex items-center justify-center">
             주요행사
           </div>
         </button>
         <button className="cursor-pointer [border:none] p-0 bg-[transparent] self-stretch flex flex-col items-center justify-center">
-          <div className="self-stretch flex-1 relative text-xs leading-[16px] font-headings-headling-2 text-text-gray text-center flex items-center justify-center">
+          <div className="self-stretch flex-1 relative text-sm leading-[20px] font-body-text-small-text-bold text-text-gray text-center flex items-center justify-center">
             회원동정
           </div>
         </button>
-        <button className="cursor-pointer [border:none] p-0 bg-[transparent] self-stretch hidden flex-col items-center justify-center">
-          <div className="self-stretch flex-1 relative text-xs leading-[16px] font-headings-headling-2 text-text-gray text-center flex items-center justify-center">
-            Theme
-          </div>
-        </button>
       </div>
-      <div className="w-full flex flex-row items-start justify-center gap-[18px] max-w-[1300px] text-sm text-bg-colors-white md:flex-col md:items-center md:justify-center md:pl-10 md:pr-10 md:box-border mq420:pl-0 mq420:pr-0 mq420:box-border">
+      <div className="w-full flex flex-row items-start justify-center gap-[18px] max-w-[1300px] text-sm text-bg-white md:flex-col md:items-center md:justify-center md:pl-10 md:pr-10 md:box-border mq420:pl-0 mq420:pr-0 mq420:box-border">
         <div className="w-[420px] flex flex-col items-start justify-start py-5 px-0 box-border gap-[18px] min-w-[300px] md:self-stretch md:w-auto">
           <div className="rounded-2xl bg-colors-cyan-800 flex flex-row items-center justify-center py-0.5 px-2">
             <div className="relative leading-[20px] mq420:text-xs">
               주요행사
             </div>
           </div>
-          <div className="self-stretch flex flex-col items-start justify-start gap-[8px] text-left text-xs text-text-title-small-light">
+          <div className="self-stretch flex flex-col items-start justify-start gap-[16px] text-left text-xs text-text-text-small-light">
             <div className="self-stretch flex flex-col items-start justify-start">
               <div className="relative leading-[16px]">2023-12-03</div>
               <b className="self-stretch relative text-base leading-[24px] overflow-hidden text-ellipsis whitespace-nowrap mq420:text-smi">
@@ -57,7 +48,7 @@ const MainContent2: NextPage = () => {
                 (유튜브 중계 링크 포함)
               </b>
             </div>
-            <div className="self-stretch relative leading-[16px] [display:-webkit-inline-box] overflow-hidden text-ellipsis [-webkit-line-clamp:2] [-webkit-box-orient:vertical]">
+            <div className="self-stretch relative text-sm leading-[20px] [display:-webkit-inline-box] overflow-hidden text-ellipsis [-webkit-line-clamp:2] [-webkit-box-orient:vertical]">
               일정표는 첨부된 PDF 파일을 통해 확인 부탁 드립니다. 정부광고제도
               합리화를 위한 법제 및 집행 개선 방안 (유튜브 중계 링크 포함).
               정부광고제도 합리화를 위한 법제 및 집행 개선 방안 (유튜브 중계
@@ -71,7 +62,7 @@ const MainContent2: NextPage = () => {
               주요행사
             </div>
           </div>
-          <div className="self-stretch flex flex-col items-start justify-start gap-[8px] text-left text-xs text-text-title-small-light">
+          <div className="self-stretch flex flex-col items-start justify-start gap-[16px] text-left text-xs text-text-text-small-light">
             <div className="self-stretch flex flex-col items-start justify-start">
               <div className="relative leading-[16px]">2023-12-03</div>
               <b className="self-stretch relative text-base leading-[24px] overflow-hidden text-ellipsis whitespace-nowrap mq420:text-smi">
@@ -79,7 +70,7 @@ const MainContent2: NextPage = () => {
                 (유튜브 중계 링크 포함)
               </b>
             </div>
-            <div className="self-stretch relative leading-[16px] [display:-webkit-inline-box] overflow-hidden text-ellipsis [-webkit-line-clamp:2] [-webkit-box-orient:vertical]">
+            <div className="self-stretch relative text-sm leading-[20px] [display:-webkit-inline-box] overflow-hidden text-ellipsis [-webkit-line-clamp:2] [-webkit-box-orient:vertical]">
               일정표는 첨부된 PDF 파일을 통해 확인 부탁 드립니다. 정부광고제도
               합리화를 위한 법제 및 집행 개선 방안 (유튜브 중계 링크 포함).
               정부광고제도 합리화를 위한 법제 및 집행 개선 방안 (유튜브 중계
@@ -93,7 +84,7 @@ const MainContent2: NextPage = () => {
               주요행사
             </div>
           </div>
-          <div className="self-stretch flex flex-col items-start justify-start gap-[8px] text-left text-xs text-text-title-small-light">
+          <div className="self-stretch flex flex-col items-start justify-start gap-[16px] text-left text-xs text-text-text-small-light">
             <div className="self-stretch flex flex-col items-start justify-start">
               <div className="relative leading-[16px]">2023-12-03</div>
               <b className="self-stretch relative text-base leading-[24px] overflow-hidden text-ellipsis whitespace-nowrap mq420:text-smi">
@@ -101,7 +92,7 @@ const MainContent2: NextPage = () => {
                 (유튜브 중계 링크 포함)
               </b>
             </div>
-            <div className="self-stretch relative leading-[16px] [display:-webkit-inline-box] overflow-hidden text-ellipsis [-webkit-line-clamp:2] [-webkit-box-orient:vertical]">
+            <div className="self-stretch relative text-sm leading-[20px] [display:-webkit-inline-box] overflow-hidden text-ellipsis [-webkit-line-clamp:2] [-webkit-box-orient:vertical]">
               일정표는 첨부된 PDF 파일을 통해 확인 부탁 드립니다. 정부광고제도
               합리화를 위한 법제 및 집행 개선 방안 (유튜브 중계 링크 포함).
               정부광고제도 합리화를 위한 법제 및 집행 개선 방안 (유튜브 중계
@@ -110,8 +101,8 @@ const MainContent2: NextPage = () => {
           </div>
         </div>
       </div>
-      <button className="cursor-pointer py-1.5 px-4 bg-[transparent] rounded-xl overflow-hidden flex flex-row items-center justify-center border-[1px] border-solid border-button-colors-dark-sky hover:bg-gainsboro">
-        <div className="relative text-xs leading-[16px] font-headings-headling-2 text-button-colors-dark-sky text-center mq420:text-2xs">
+      <button className="cursor-pointer py-1.5 px-4 bg-[transparent] rounded-xl overflow-hidden flex flex-row items-center justify-center border-[1px] border-solid border-bg-primary hover:bg-gainsboro-100">
+        <div className="relative text-xs leading-[16px] font-body-text-small-text-bold text-bg-primary text-center mq420:text-2xs">
           더보기
         </div>
       </button>
